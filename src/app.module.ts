8c4e1fc1abd8f95001eadc268d0ebe06b1625888
lonewolfyx <877085893@qq.com>
2025-03-10 22:10:35 +0800

    feat: 增加共享模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ShareModule } from './share/share.module'
 
 @Module({
-	imports: [],
+	imports: [ShareModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
