63b0583ccc9906d9a233a6e9e69599ae83a52c10
lonewolfyx <877085893@qq.com>
2025-03-11 16:24:52 +0800

    feat: add database module

---
+++ src/share/database/database.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit } from '@nestjs/common'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit {
+
+    constructor() {
+        super({
+            log: [
+                {
+                    emit: 'event',
+                    level: 'query'
+                }
+            ]
+        })
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        this.$on('query', (e: Prisma.QueryEvent) => {
+            console.log(e)
+        })
+    }
+
+    async onModuleInit() {
+        await this.$connect()
+    }
+}
--- src/share/share.module.ts
+++ src/share/share.module.ts
@@ -1,8 +1,10 @@
 import { Global, Module } from '@nestjs/common'
+import { DatabaseModule } from '@/share/database/database.module'
 
 @Global()
 @Module({
-	imports: [],
-	exports: []
+    imports: [DatabaseModule],
+    exports: [DatabaseModule]
 })
-export class ShareModule {}
+export class ShareModule {
+}
