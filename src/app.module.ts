7848e83446511da67b5a06793a2d9cd0c9b8fd48
lonewolfyx <877085893@qq.com>
2025-03-20 15:24:56 +0800

    feat: 新增全局配置读取模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { ShareModule } from './share/share.module'
+import { ShareModule } from '@/share/share.module'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { ConfigModule } from '@nestjs/config'
+import * as process from 'node:process'
 
 @Module({
-	imports: [ShareModule],
-	controllers: [AppController],
-	providers: [AppService]
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: [`.env.${process.env.NODE_ENV}`]
+        }),
+        ShareModule
+
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
-export class AppModule {}
+export class AppModule {
+}
